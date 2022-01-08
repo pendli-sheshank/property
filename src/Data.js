b89a3982a0886baaa99a9ad454e9136931627667
sheshank <sheshank3336@gmail.com>
2022-01-08 22:29:41 +0530

    done

---
+++ src/Data.js
@@ -0,0 +1,8 @@
+const Data = [
+  "https://www.officespacesoftware.com/wp-content/uploads/image_1280px_89c97a527b494316a324a34fa1b7c079.jpeg",
+  "https://www.officespacesoftware.com/wp-content/uploads/image_1280px_3151304004404a7b995baa7d0dc2570a.jpeg",
+  "https://www.officespacesoftware.com/wp-content/uploads/image_1280px_a7fdb7ec9858479ebefe916c992829e6.jpeg",
+  "https://www.wework.com/ideas/wp-content/uploads/sites/4/2021/02/Web_150DPI-20210124_WeWork_Brand-Shoot_San-Francisco_002_v1-1120x630.jpg",
+  "https://www.executivecentre.com/wp-content/uploads/2020/05/2pp-1400.jpg",
+];
+export default Data;
---
+++ src/components/ImagesContainer/ImagesContainer.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import data from "../../Data";
+import { RiHeartAddLine } from "react-icons/ri";
+import "./index.css";
+
+const ImagesContainer = () => {
+  const [image, selectedImage] = useState(data[0]);
+  const [bgColor, setBgColor] = useState();
+  const [bookNow, bookingDone] = useState();
+
+  const wishListtoggle = () => {
+    setBgColor(!bgColor);
+  };
+
+  const bookingToggle = () => {
+    bookingDone(!bookNow);
+  };
+
+  return (
+    <div className="images-container">
+      <div className="left-image-container">
+        {data.map((i, index) => (
+          <img
+            key={index}
+            style={{ border: image === i ? "3px solid black" : "" }}
+            src={i}
+            onClick={() => selectedImage(i)}
+            alt={i}
+            className="side-image card"
+          />
+        ))}
+      </div>
+      <div className="right-image-container">
+        <img className="main-image card" src={image} alt="view" />
+      </div>
+      <div className="content-container">
+        <h1>Meeting Room</h1>
+        <p>Avalible</p>
+        <div className="day-container">
+          <div className="day">
+            <label htmlFor="startdate">Start Date</label>
+            <br />
+            <input type="date" className="form-control" name="startdate" />
+          </div>
+          <div className="day">
+            <label htmlFor="enddate">End Date</label>
+            <br />
+            <input type="date" className="form-control" name="enddate" />
+          </div>
+        </div>
+
+        <div className="input">
+          <label className="label" htmlFor="seats">
+            No. of seats
+          </label>
+          <input name="seats" type="number" />
+        </div>
+        <div className="booking">
+          <span>Rs.8000/day</span>
+          <button onClick={bookingToggle}>
+            {bookNow ? "Cancle" : "Book Now"}
+          </button>
+          <RiHeartAddLine
+            onClick={wishListtoggle}
+            className={bgColor ? "purple" : "white"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImagesContainer;
